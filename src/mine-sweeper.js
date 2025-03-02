d4a1f535ced83862ee88a6a84d21d6f6a5c72398
Monte_alien <monah445@gmal.com>
2025-03-02 22:41:50 +0100

    fix:all tasks

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+    let ans = [];
+    for (let i = 0; i < matrix.length; i += 1) {
+        let line = [];
+        for (let j = 0; j < matrix[0].length; j += 1) {
+            let mines_around = 0;
+
+            let mines_top = (matrix[i - 1] === undefined) ? 0 : Number(matrix[i - 1][j]);
+            let mines_left = (matrix[i][j - 1] === undefined) ? 0 : Number(matrix[i][j - 1]);
+            let mines_bot = (matrix[i + 1] === undefined) ? 0 : Number(matrix[i + 1][j]);
+            let mines_right = (matrix[i][j + 1] === undefined) ? 0 : Number(matrix[i][j + 1]);
+
+            let mines_top_left = (matrix[i - 1] === undefined) ? 0 : (matrix[i - 1][j - 1] === undefined) ? 0 : Number(matrix[i - 1][j - 1]);
+            let mines_top_right = (matrix[i - 1] === undefined) ? 0 : (matrix[i - 1][j + 1] === undefined) ? 0 : Number(matrix[i - 1][j + 1]);
+            let mines_bot_left = (matrix[i + 1] === undefined) ? 0 : (matrix[i + 1][j - 1] === undefined) ? 0 : Number(matrix[i + 1][j - 1]);
+            let mines_bot_right = (matrix[i + 1] === undefined) ? 0 : (matrix[i + 1][j + 1] === undefined) ? 0 : Number(matrix[i + 1][j + 1]);
+
+            mines_around = mines_top + mines_left + mines_bot + mines_right
+                + mines_top_left + mines_top_right + mines_bot_left + mines_bot_right;
+            console.log(`summ ${ mines_around }`);
+            line.push(mines_around);
+        }
+        ans.push(line);
+    }
+    return ans;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    offset = 97;
+    reverseType = false;
+    constructor(type) {
+        if (!type) this.reverseType = true;
+        this.crypting = this.crypting.bind(this);
+    }
+
+    encrypt(mess, key) {
+        if (mess === undefined || key === undefined) {
+            throw new Error("Incorrect arguments!");
+        }
+        return this.crypting(mess, key, true);
+    }
+
+    decrypt(mess, key) {
+        if (mess === undefined || key === undefined) {
+            throw new Error("Incorrect arguments!");
+        }
+        return this.crypting(mess, key, false);
+    }
+
+    crypting(mess, key, streight) {
+        let validation = -1;
+        const key_arr = key.split('').map((k) => k.toLowerCase().charCodeAt(0) - this.offset);
+        const text = mess.split('').map((e) => {
+            if (e.toLowerCase() !== e.toUpperCase()) {
+                e = e.toLowerCase().charCodeAt(0) - this.offset;
+                (validation < key_arr.length - 1) ? validation += 1 : validation = 0;
+                let x = key_arr[validation];
+                if (streight) e = (e + x) % this.alphabet.length;
+                if (!streight) e = (((e - x) > -1) && ((e - x) < this.alphabet.length)) ? (e - x) : (e - x + this.alphabet.length);
+                e = String.fromCodePoint(e + this.offset).toUpperCase();
+            }
+            return e;
+        });
+        if (!this.reverseType)return text.reverse().join('');
+        return text.join('');
+    }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+    VigenereCipheringMachine
 };
